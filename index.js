bbbc5c262d4a8907d529ed2dd2cfad75343b8e28
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2021-05-02 17:09:50 +0300

    basics to calculate buy|sell limit orders

--- index.js
+++ index.js
@@ -7,47 +7,55 @@ const tick = async(config, binanceClient) => {
     const { asset, base, spread, allocation } =  config;
     const market = `${asset}/${base}`;
 
-    const balances = await binanceClient.fetchBalance();
-    console.log('Binance balance', balances);
-
-    // console.log(asset, base);
-
-    // const order = await binanceClient.fetchOpenOrders(market);
-    // order.forEach(async order => {
-    //     await binanceClient.cancelOrder(order.id);
-    // });
-
-    // const results = await Promise.all([
-    //     axios.get(
-    //         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-    //     ),
-    //     axios.get(
-    //         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=dogecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-    //     ),
-    // ]);
-
-    // console.log('Coingecko results', results);
-
-    // const marketPrice = results[0].data.bitcoin.usd / results[1].dogecoin.usd;
-
-    // const buyPrice = marketPrice * (1 - spread);
-    // const sellPrice = marketPrice * (1 + spread);
-
+    // check Binance balance 
     // const balances = await binanceClient.fetchBalance();
-    // const assetBalance = balances.free[asset];
-    // const baseBalance = balances.free[base];
+    // console.log('Free asset (DOGE)', balances.free[asset]);
+    // console.log('Free base (USDT)', balances.free[asset]);
 
-    // const sellVolume = assetBalance * allocation;
-    // const buyVolume = (baseBalance + allocation) / marketPrice;
+    /** cancel previously scheduled (limit) orders for the market */
+    const order = await binanceClient.fetchOpenOrders(market);
+    order.forEach(async order => {
+        await binanceClient.cancelOrder(order.id);
+    });
 
-    // await binanceClient.createLimitSEllOrder(market, sellVolume, sellPrice);
+    const coingeckoPrices = await Promise.all([
+        axios.get(
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        ),
+        axios.get(
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&ids=dogecoin&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        ),
+    ]);
+
+    // console.log('Coingecko prices', coingeckoPrices[0].data[0], coingeckoPrices[1].data[0], coingeckoPrices[0].data[0].current_price, coingeckoPrices[1].data[0].current_price);
+
+    const USD = coingeckoPrices[0].data[0].current_price;
+    const DOGE = coingeckoPrices[1].data[0].current_price;
+
+    console.log('USD', USD);
+    console.log('DOGE', DOGE);
+
+    /** calculate sell / buy orders */
+    const marketPrice = USD / DOGE;
+
+    const buyPrice = marketPrice * (1 - spread);
+    const sellPrice = marketPrice * (1 + spread);
+
+    const balances = await binanceClient.fetchBalance();
+    const assetBalance = balances.free[asset];
+    const baseBalance = balances.free[base];
+
+    const sellVolume = assetBalance * allocation;
+    const buyVolume = (baseBalance + allocation) / marketPrice;
+
+    console.log(`
+        Tick for ${market}...
+        Limit sell order for ${sellVolume}@${sellPrice}
+        Limit buy order for ${buyVolume}@${buyPrice}
+    `);
+
+    // await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
     // await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice);
-
-    // console.log(`
-    //     Tick for ${market}...
-    //     Limit sell order for ${sellVolume}@${sellPrice}
-    //     Limit buy order for ${buyVolume}@${buyPrice}
-    // `);
 };
 
 const run = () => {
@@ -56,7 +64,7 @@ const run = () => {
         base: 'USDT',
         spread: 0.2,
         allocation: 0.1,
-        tickInterval: 2000,
+        tickInterval: 5000,
     };
 
     const binanceClient = new ccxt.binance({
@@ -67,7 +75,7 @@ const run = () => {
     tick(config, binanceClient);
 
     // TODO: enable continous trading
-    // setInterval(tick, config.tickInterval, config, binanceClient);
+    setInterval(tick, config.tickInterval, config, binanceClient);
 };
 
 /** invoke crypto trading bot */
